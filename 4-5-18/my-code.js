e739556c80d5be56db2e23cf4a169fe13ad59fcd
Eddie Montooth <edmon@udel.edu>
2018-04-05 10:36:58 -0400

    JS 2

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,13 @@
+blist = ["end of the semester", "no research", "Disen gets better"];
+console.log(blist.length);
+blist.push("get a beer");
+console.log(blist.length);
+blist.pop();
+blist.pop();
+console.log(blist.length);
+console.log(blist);
+
+//for loop
+for ( i = 0; i < 4; i++) {
+  console.log(blist[i]);
+}
